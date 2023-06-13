165a7432ec4276334b889b8c5c5e097d1356716c
Traian <traian30y@yahoo.co.uk>
2023-06-13 14:14:57 +0100

    update 13.06.23 ac1

--- activities/src/bin/ac1.rs
+++ activities/src/bin/ac1.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world");
+    // shadowing
+
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("x in the inner scope = {}", x);
+    }
+    println!("x in the outer scope = {}", x);
+
+    // compound types - tuples and arrays
+    let tup = (500, 1.45, true);
+
+    // destructuring
+    let (x, _y, z) = tup;
+    println!("The value of x from the tuple is {}", x);
+
+    // accesing a tuple by . notation
+    println!("The value of z from the tuple is : {}", z);
+
+    // the array type, unlike the tuple, every element of the array has the same type
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", a);
 }
