48ac714091d830d402838c214710ba3b0384b6bd
Traian <traian30y@yahoo.co.uk>
2023-07-25 13:30:54 +0100

    update from t14

--- activities/src/bin/ac0.rs
+++ activities/src/bin/ac0.rs
@@ -4,4 +4,13 @@ fn main() {
     // x = 6; not working - cannot assign twice to immutable variable(we need to make it mutable by placing mut in front)
     x = 6;
     println!("The value of x is: {}", x);
+
+    // shadowing - you can declare a new variable with the same name as the previous variable
+    let y = 5;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {}", y); // 12
+    }
+    println!("The value of y is: {}", y); // 6
 }
