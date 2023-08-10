47c4c44ef2c8f67bc34a2b9a63c1d79d3e906df6
Traian <traian30y@yahoo.co.uk>
2023-08-10 01:30:36 +0100

    ac9 string slices - start

---
+++ activities/src/bin/ac9_slice_type.rs
@@ -0,0 +1,6 @@
+// Slices let you reference a contiguous sequence of elements in a collection rather than the whole collection
+// A slice is a kind of reference, so it does not have ownership.
+
+fn main() {
+    // a string slice is a reference to a part of a String
+}
