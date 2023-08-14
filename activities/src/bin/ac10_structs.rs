617f720ab5ecb571253407598103db397fccdf19
Traian <traian30y@yahoo.co.uk>
2023-08-14 14:47:41 +0100

    ac10 structs start

--- activities/src/bin/ac10_structs.rs
+++ activities/src/bin/ac10_structs.rs
@@ -1,10 +1,19 @@
 // struct - custom data type that lets you package together and name multiple related values that make up a meaningful group
 struct User {
     active: bool,
+    // in the User struct definition, we used the owned String rather than athe string slice type .&str
+    // this is deliberate, because we want each instance of this struct to own all of its data
+    // it is also possible for structs to store references to data owned by something else, but to do so require the use of lifetimes
     username: String,
     email: String,
     sign_in_count: u64,
 }
+// using tuple structs without named fields to create diffrent types
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+// uni-like structs - can be useful when you need to implement a trait on some type but you don't have any data that you want to store in the type itself
+struct AlwaysEqual;
+
 fn main() {
     // creating an instance of the struct
     let mut user1 = User {
@@ -21,8 +30,17 @@ fn main() {
     // in this example we can no longer use user1 after creating user2 because the string in the username field of user1 was moved into user2
     let user2 = User {
         email: String::from("anacumere@example.com"),
+        // the ..user1 must come last to specify that any remaining fields should get their values from user1
         ..user1
     };
+
+    // tuple structs - useful when you want to give the whole tuple a name and make the tuple a diffrent type from other tuples,
+    // and when naming each field would be redundant
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    // uni-like struct
+    let subject = AlwaysEqual;
 }
 fn build_user(email: String, username: String) -> User {
     User {
