b12672baf143173da46c0a6c39030e741e146f5d
Traian <traian30y@yahoo.co.uk>
2023-08-14 22:31:33 +0100

    ac12 struct methods

---
+++ activities/src/bin/ac12_structs.rs
@@ -0,0 +1,30 @@
+// struct methods
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+// unlike functions, methods are defined within the context of a struct, and their first parameter is always self, which represents the instance of the struct the method its being called on.
+impl Rectangle {
+    // &self short for self: &Self
+    // methods can take ownership of self, borrow self immutably, or borrow self mutably
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    // getters are useful  because you can make the field private, but the method public, and thus enable read-only access to that field as a part of the type's public API.
+}
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("the area of the rectangle is {}", rect1.area());
+    if rect1.width() {
+        println!("The rectangle has a nonzero width of {}", rect1.width);
+    }
+}
+
+// when you call a method with object.somethiong(), Rust automatically adds in &, &mut or * so object matches the signature of the method
--- activities/src/bin/ac8_references_and_borrowing.rs
+++ activities/src/bin/ac8_references_and_borrowing.rs
@@ -6,7 +6,7 @@
 fn main() {
     let s1 = String::from("hello");
 
-    // the &s1 syntaxlet us create a reference that refers to the value of s1 but does not own it.
+    // the &s1 syntax lets us create a reference that refers to the value of s1 but does not own it.
     // Because it does not own it, the value it points to will not be dropped when the reference stops being used.
     let len = calculate_length(&s1);
     println!("The length of '{s1}' is {len}.");
@@ -14,7 +14,7 @@ fn main() {
 
 fn calculate_length(s: &String) -> usize {
     s.len()
-} // here s goes out of scope. But because it does not have ownershipof what it refers to, the String is not dropped.
+} // here s goes out of scope. But because it does not have ownership of what it refers to, the String is not dropped.
 
 // when functions have references as parameters instead of the actual values, we won't need to return the values in order to give back ownership, because we never had ownership.
 // we call the action of creating a reference borrowing. If we try to modify something we're borrowing it won't work.
