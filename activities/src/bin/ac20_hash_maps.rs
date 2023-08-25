d589330cd2d2351254f0ef04e71db3a7542a9a8a
Traian <traian30y@yahoo.co.uk>
2023-08-25 23:50:00 +0100

    update ac20_hash_maps

---
+++ activities/src/bin/ac20_hash_maps.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Storing keys with associated values in hash maps
+    // The type HashMap<K, V> stores a mapping of keys of type K to valueas of type V using a hashing function.
+    // Hash maps are useful when you want to look up data not by using an index, as you can with vectors, but by using a key that can be of any type.
+
+    // Creating a new hash map
+}
