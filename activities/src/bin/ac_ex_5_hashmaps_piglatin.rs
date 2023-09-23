91cbbff6c107a125970e018199de5c958c70bb92
Traian <traian30y@yahoo.co.uk>
2023-09-23 13:40:20 +0100

    ex5 pig latin update

--- activities/src/bin/ac_ex_5_hashmaps_piglatin.rs
+++ activities/src/bin/ac_ex_5_hashmaps_piglatin.rs
@@ -1,3 +1,15 @@
 // Convert strings to pig latin. The first consonant of each word is moved to the end of the word and ay is added, so first becomes irst-fay. Words that start with a vowel have hay added to the end instead (apple becomes apple-hay). Keep in mind the details about UTF-8 encoding!
-
-fn main() {}
+use std::collections::HashMap;
+use std::io;
+fn main() {
+    let phrase = read_string();
+    println!("{phrase}");
+}
+fn read_string() -> String {
+    println!("Please enter your phrase: ");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line!");
+    input
+}
