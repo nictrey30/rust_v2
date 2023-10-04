867cf960540823ec11f4df869498f301ccb81b92
Traian <traian30y@yahoo.co.uk>
2023-10-04 12:41:04 +0100

    error handling

---
+++ activities/src/bin/ac22_eh.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::ErrorKind;
+fn main() {
+    // panic!("crash and burn");
+    // let v = vec![1, 2, 3];
+    // v[99];
+
+    // File::open returns a Result value because the function could fail
+    // The return type of File::open is a Result<T, E>. The generic parameter T has been filled in by the implementation of File::open with the type of the success value, std::fs::File, which is a file handle. The type of E used in the error value is std::io::Error.
+    let greeting_file_result = File::open("hello.txt");
+
+    // this code will panic! no matter what why File::open failed
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => {
+    //         panic!("Problem opening the file: {:?}", error)
+    //     }
+    // };
+
+    // matching on diffrent errors
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+}
