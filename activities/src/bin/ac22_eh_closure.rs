adea8ed8b81f93ce9defb281756526e1879f68b2
Traian <traian30y@yahoo.co.uk>
2023-10-07 14:09:54 +0100

    error handling

--- activities/src/bin/ac22_eh_closure.rs
+++ activities/src/bin/ac22_eh_closure.rs
@@ -1,3 +1,13 @@
 use std::fs::File;
 use std::io::ErrorKind;
-fn main() {}
+fn main() {
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
